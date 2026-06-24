f475e4f6605d2b9daa629b952812586f47c04686
samuelscheit <github@samuelscheit.com>
2026-06-24 18:06:35 +0200

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -11,21 +11,13 @@ const nextConfig = {
 	experimental: {
 		scrollRestoration: true,
 	},
-	webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/i,
 			use: ["@svgr/webpack"],
 		});
 
 		return config;
-	},
-	turbopack: {
-		rules: {
-			"*.svg": {
-				loaders: ["@svgr/webpack"],
-				as: "*.js",
-			},
-		},
 	},
 	output: "export",
 	images: {
